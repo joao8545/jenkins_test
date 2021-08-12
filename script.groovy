557ea4695c87a80aa49da92e91af4cdbac3d353b
João Anastácio da Rocha Almeida <almeida.jar97@gmail.com>
2021-08-12 14:50:53 +0200

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,15 @@
+pipelineJob('example') {
+    definition {
+        cps {
+          script('''pipeline{
+          |  agent any
+          |  stages{
+          |   stage('stage1') {
+          |     echo 'hello world'
+          |   }
+          |  }
+          |}'''.stripMargin())
+            sandbox()
+        }
+    }
+}
