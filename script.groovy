434e7eeac34fde1f6c9071d35d36fa9a7307af4f
Joao Anastacio <joao.anastacio.almeida@ericsson.com>
2021-08-12 16:07:54 +0200

    commit

--- script.groovy
+++ script.groovy
@@ -1,17 +1,28 @@
-pipelineJob('example') {
+pipelineJob('example1') {
     definition {
-        cps {
-          script('''
-          |pipeline{
-          |     agent any
-          |     stages{
-          |         stage('stage1') {
-          |             steps{       
-          |                 echo 'hello world!'
-          |             }
-          |         }
-          |     }
-          |}'''.stripMargin())
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        credentials "${GITHUB_CRED_USR}"
+                        url "https://github.com/${GITHUB_CRED_USR}/jenkins_test"
+                    }
+                    
+                }
+            }
+
+
+            script('''
+            |pipeline{
+            |   agent any
+            |   stages{
+            |       stage('stage1') {
+            |           steps{       
+            |               echo 'hello world!'
+            |           }
+            |       }
+            |   }
+            |}'''.stripMargin())
             sandbox()
         }
     }
