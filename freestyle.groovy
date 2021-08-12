2dd60fdcc54fad6eec3a541e2ea43148aec6d770
João Anastácio da Rocha Almeida <almeida.jar97@gmail.com>
2021-08-12 16:58:22 +0200

    Update freestyle.groovy

--- freestyle.groovy
+++ freestyle.groovy
@@ -4,11 +4,12 @@ freeStyleJob('example1') {
         github('joao8545/jenkins_test')
     }
     steps{
-        groovyScriptFile("freestyle")
+        
         python{
             command("print('Hello World')")
         }
         shell("touch a")
+        groovyScriptFile("freestyle")
     }    
     triggers {
         githubPush()
